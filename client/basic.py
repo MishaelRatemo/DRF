36304c4be2ca94e7bd98e9fc329da9c6a8f78a74
MishaelRatemo <ratemomishael@gmail.com>
2022-06-16 15:15:49 +0300

    addded client file to test APi

---
+++ client/basic.py
@@ -0,0 +1,8 @@
+import requests 
+
+endpoint1 ='https://httpbin.org/status/200/'
+endpoint1 ='https://httpbin.org/anything '
+endpoint2 ='http://127.0.0.1:8000'
+
+get_response=requests.get(endpoint2) # make http reques
+print(get_response.text)
