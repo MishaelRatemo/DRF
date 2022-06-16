62a2c8e20d65fbd16c1f60ace1489e38ffd25827
MishaelRatemo <ratemomishael@gmail.com>
2022-06-16 15:33:43 +0300

    changed to give a response in json format

--- client/basic.py
+++ client/basic.py
@@ -2,7 +2,8 @@ import requests
 
 endpoint1 ='https://httpbin.org/status/200/'
 endpoint1 ='https://httpbin.org/anything '
-endpoint2 ='http://127.0.0.1:8000'
+endpoint2 ='http://127.0.0.1:8000/api'
 
 get_response=requests.get(endpoint2) # make http reques
-print(get_response.text)
+# print(get_response.text)
+print(get_response.json()['message'])
