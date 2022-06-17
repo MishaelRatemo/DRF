0cc0d610b8a1217a434824862902054bf43e17a6
MishaelRatemo <ratemomishael@gmail.com>
2022-06-17 11:13:24 +0300

    modified the api view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,12 @@
 from django.http import JsonResponse
 
+from products.models import Product
+
 def api_home(request, *args, **kwargs):
-    return JsonResponse ({
-        'message': 'Hello, sampple API response!!!'
-    })
+    model_data = Product.objects.all().order_by('?').first()
+    data ={}
+    if model_data:
+        data['title']=model_data.title
+        data['content']=model_data.content
+        data['price']=model_data.price
+    return JsonResponse (data)
