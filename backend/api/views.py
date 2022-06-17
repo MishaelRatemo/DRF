725fe305cdfe741b8f985907863cdb16c94ad5cd
MishaelRatemo <ratemomishael@gmail.com>
2022-06-17 12:17:23 +0300

    used model_to dict method from dj to make dict

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,4 @@
+from django.forms import model_to_dict
 from django.http import JsonResponse
 
 from products.models import Product
@@ -6,7 +7,8 @@ def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by('?').first()
     data ={}
     if model_data:
-        data['title']=model_data.title
-        data['content']=model_data.content
-        data['price']=model_data.price
+        # data['title']=model_data.title
+        # data['content']=model_data.content
+        # data['price']=model_data.price
+        data= model_to_dict(model_data, fields=['id','title','content', 'price'])
     return JsonResponse (data)
