af8c29af4f9fc87461b1a9e0407da085790eb917
MishaelRatemo <ratemomishael@gmail.com>
2022-06-17 17:58:34 +0300

    drf generics createAPIView

--- backend/products/views.py
+++ backend/products/views.py
@@ -3,6 +3,21 @@ from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
 
+# can be use as a post method making work easy
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    # def perform_create(self, serializer):
+    #     # serializer.save(user=self.request.user)
+    #     title = serializer.validated_data.get('title')
+    #     content = serializer.validated_data.get('data')
+        
+    #     if content is None:
+    #         content =title
+    #     serializer.save(content=content)
+        # you can send a Django signal here
+
 class ProductDetailsAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
