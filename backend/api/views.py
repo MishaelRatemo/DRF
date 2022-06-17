2fe404dd4f0d4ed06fc7f38c13f33a8bb388f885
MishaelRatemo <ratemomishael@gmail.com>
2022-06-17 13:16:43 +0300

    Added bot GET and POSt method using restframework

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,29 @@
 from django.forms import model_to_dict
 from django.http import JsonResponse
+from yaml import serialize
 
 from products.models import Product
+from products.serializers import ProductSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+
+@api_view(['GET'])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by('?').first()
+    ''' DRF API view
+    '''
+    instance = Product.objects.all().order_by('?').first()
     data ={}
-    if model_data:
-        # data['title']=model_data.title
-        # data['content']=model_data.content
-        # data['price']=model_data.price
-        data= model_to_dict(model_data, fields=['id','title','content', 'price'])
-    return JsonResponse (data)
+    if instance:
+    #    data= model_to_dict(instance, fields=['id','title','content', 'price'])
+        data = ProductSerializer(instance).data
+    return Response (data)
+
+@api_view(['POST'])
+def api_home(request, *args, **kwargs):
+    ''' DRF API view'''
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        instance = serializer.save()
+        print(instance)
+        return Response (serializer.data)
